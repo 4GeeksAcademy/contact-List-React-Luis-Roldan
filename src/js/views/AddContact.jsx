6b748e575d4c1f83a7a0ac0934338eb3cb8a4e90
Luis-Roldan <108189366+Luis-Roldan@users.noreply.github.com>
2023-11-23 21:46:20 +0000

    html aa css

--- src/js/views/AddContact.jsx
+++ src/js/views/AddContact.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import "../../styles/AddContact.css";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const AddContact = () => {
-  <div className="text-center mt-5">
-    <form action=""></form>
-  </div>;
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div className="mt-5">
+      <h1 className="text-center">Add a New Contact</h1>
+      <div className="FormContainer">
+        <div className="InputName">
+          <div class="container">
+            <span>
+              <strong>Full Name</strong>{" "}
+            </span>
+            <div class="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label for="floatingInput">Full Name</label>
+            </div>
+          </div>
+        </div>
+        <div className="InputEmail">
+          <div class="container">
+            <span>
+              <strong>Enter Email</strong>{" "}
+            </span>
+            <div class="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label for="floatingInput">Enter email</label>
+            </div>
+          </div>
+        </div>
+        <div className="InputPhone">
+          <div class="container">
+            <span>
+              <strong>Phone</strong>{" "}
+            </span>
+            <div class="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label for="floatingInput">Enter phone</label>
+            </div>
+          </div>
+        </div>
+        <div className="InputAddress">
+          <div class="container">
+            <span>
+              <strong>Address</strong>{" "}
+            </span>
+            <div class="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label for="floatingInput">Enter address</label>
+            </div>
+          </div>
+        </div>
+        <button className="btn btn-primary SaveButton">Save</button>
+        <Link to="/" className="link-opacity-100">
+          or get back to contacts
+        </Link>
+      </div>
+    </div>
+  );
 };
--- src/js/views/Contact.jsx
+++ src/js/views/Contact.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "../../styles/Contact.css";
 import { ContactCard } from "../component/ContactCard.jsx";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Contact = () => {
+  const navigate = useNavigate();
+
   return (
     <div className=" mt-5">
       <div className="ContactsContainer d-flex flex-row-reverse mx-2 mt-2">
-        <button className="btn btn-primary AddContactButton">
-          Add New Contact
-        </button>
+        <Link to="/form">
+          <button className="btn btn-primary AddContactButton">
+            Add new contact
+          </button>
+        </Link>
       </div>
       <div className="ContactsList">
         <ContactCard />
